5cfc15cd687dd64c7e2f274c428c960b99f3a22a
Donald L <dfleon2011@gmail.com>
2023-08-27 18:26:31 -0400

    update server

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.static("public"));
-app.use('/api', api_routes);
 app.use('/', html_routes);
+app.use('/api', api_routes);
 
 app.listen(PORT,()=>{
     console.log(`Server running on http://localhost:${PORT}`)
