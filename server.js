8aadbe40b4587693be22c8af9192ab68839f3e0b
Donald L <dfleon2011@gmail.com>
2023-08-27 18:21:29 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const html_route = require(html-routes);
-const api_routes = require(api-routes);
+const path = require('path');
+const fs = require('fs')
+const html_routes = require('./routes/html-routes');
+const api_routes = require('./routes/api-routes');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -8,8 +10,8 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(html_routes);
-app.use(api_routes);
+app.use('/api', api_routes);
+app.use('/', html_routes);
 
 app.listen(PORT,()=>{
     console.log(`Server running on http://localhost:${PORT}`)
